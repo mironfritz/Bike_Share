4d5962b9b882c87b842036407b8df1ad9bc355ef
Millan Garcia <millangg17@gmail.com>
2020-07-16 17:39:49 +0200

    styling the calendar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_07_16_085712) do
 
   # These are extensions that must be enabled in order to support this database
@@ -46,9 +45,8 @@ ActiveRecord::Schema.define(version: 2020_07_16_085712) do
     t.string "bike_type"
     t.float "latitude"
     t.float "longitude"
-    t.string "address"
     t.integer "price_per_day"
-
+    t.string "address"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
